999dbda8e0d4ef70d3c8aad8c6fce73e17619eaf
georginamusonda <georginamusonda97@gmail.com>
2025-09-19 16:51:03 +0200

    chat bot.py

--- chat bot.py
+++ chat bot.py
@@ -1,12 +1,13 @@
 
-def new_func():
     name = input("What is your name? ")
 
     print("Hello", name + "! Nice to meet you.")
-
     feeling = input("How are you feeling today? ")
-
     if feeling.lower() == "good":
         print("That's great to hear! Keep smiling, " + name + "!")
+    elif feeling.lower() =="bad":
+        print("Im sorry to  hear that, ".i hope things get better soon.")
+    else:
+        print("thanks you for sharing," + name +".")
 
-new_func()
+
